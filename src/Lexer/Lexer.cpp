655947a6e6e09eb41e2a2d1d9b1cf736096ade50
Ibrahim Kaikaa <ibrahim@flexfret.com>
2023-11-17 08:48:51 +0100

    refactor: code organization in Lexer::parse()

--- src/Lexer/Lexer.cpp
+++ src/Lexer/Lexer.cpp
@@ -5,22 +5,24 @@ std::vector<Token> Lexer::parse(const std::string &line) {
     std::vector<Token> result;
     std::string currentToken;
 
+    const std::string separators = "() ";
     for (auto c: line) {
-        if (c == '(' || c == ')' || c == ' ') {
-            if (!currentToken.empty()) {
-                if (strspn(currentToken.c_str(), "0123456789")) {
-                    result.push_back(Integer(std::stoi(currentToken)));
-                } else {
-                    result.push_back(Symbol(currentToken));
-                }
-            }
-
-            currentToken = "";
-            if (c == '(') result.push_back(OpenBracket());
-            else if (c == ')') result.push_back(ClosedBracket());
-        } else {
+        if (separators.find(c) == std::string::npos) {
             currentToken.append(1, c);
+            continue;
         }
+
+        if (!currentToken.empty()) {
+            if (strspn(currentToken.c_str(), "0123456789")) {
+                result.push_back(Integer(std::stoi(currentToken)));
+            } else {
+                result.push_back(Symbol(currentToken));
+            }
+        }
+
+        currentToken = "";
+        if (c == '(') result.push_back(OpenBracket());
+        else if (c == ')') result.push_back(ClosedBracket());
     }
 
     return result;
