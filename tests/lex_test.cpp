aaaf481c61de1efce3a7fc09f37ec747366511ac
Ibrahim Kaikaa <ibrahim@flexfret.com>
2023-11-17 08:21:14 +0100

    test: improve tests in lex_test.cpp

--- tests/lex_test.cpp
+++ tests/lex_test.cpp
@@ -2,7 +2,7 @@
 #include "../src/Tokens/Tokens.h"
 #include "../src/Lexer/Lexer.h"
 
-TEST(lex_test, ShouldSingleCharsParseIntoTokens) {
+TEST(lex_test, ShouldParseSingleCharsIntoTokens) {
     std::string sample = "(+ 1 2)";
     std::vector<Token> tokens = {
             OpenBracket(),
@@ -17,13 +17,28 @@ TEST(lex_test, ShouldSingleCharsParseIntoTokens) {
     EXPECT_EQ(tokens == result, true);
 }
 
-TEST(lex_test, ShouldMultipleCharsParseIntoTokens) {
+TEST(lex_test, ShouldParseMultipleCharsIntoTokens) {
     std::string sample = "(multiply 1 2)";
     std::vector<Token> tokens = {
             OpenBracket(),
             Symbol("multiply"),
             Integer(1),
             Integer(2),
+            ClosedBracket()
+    };
+
+    auto result = Lexer::parse(sample);
+
+    EXPECT_EQ(tokens == result, true);
+}
+
+TEST(lex_test, ShouldParseLargeNumbersIntoTokens) {
+    std::string sample = "(+ 13 54)";
+    std::vector<Token> tokens = {
+            OpenBracket(),
+            Symbol("+"),
+            Integer(13),
+            Integer(54),
             ClosedBracket()
     };
 
