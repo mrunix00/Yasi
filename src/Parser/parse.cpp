16bf353f1a66f3223d8db47a6cca8f4fb3cbff92
mrunix00 <mrunix00@protonmail.com>
2023-11-21 18:30:24 +0100

    refactor: some code organization

--- src/Parser/parse.cpp
+++ src/Parser/parse.cpp
@@ -4,14 +4,14 @@
 #include "SyntaxTreeNode.h"
 
 SyntaxTreeNode parse(const std::vector<Token> &tokens) {
-    Token operation;
     std::stack<Token> operators_stack;
     std::stack<std::vector<SyntaxTreeNode>> nodes_stack;
 
     for (const auto &token: tokens) {
-        if (token.type == Token::OpenBracket
-            || operators_stack.top().type == Token::OpenBracket) {
-            if (token.type == Token::OpenBracket) nodes_stack.emplace();
+        if (token.type == Token::OpenBracket) {
+            nodes_stack.emplace();
+            operators_stack.push(token);
+        } else if (operators_stack.top().type == Token::OpenBracket) {
             operators_stack.push(token);
         } else if (token.type == Token::ClosedBracket) {
             auto args = nodes_stack.top();
@@ -21,8 +21,8 @@ SyntaxTreeNode parse(const std::vector<Token> &tokens) {
                     operators_stack.top(),
                     args
             );
-            operators_stack.pop();
-            operators_stack.pop();
+            operators_stack.pop(); // pop the operator
+            operators_stack.pop(); // pop the bracket
         } else {
             nodes_stack.top().emplace_back(token);
         }
