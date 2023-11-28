5ed4008860f38c58f0bebab3c4481a6224302264
mrunix00 <mrunix00@protonmail.com>
2023-11-28 10:23:18 +0100

    feat: add support for nested division

---
+++ tests/divide.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "parser/SyntaxTreeNode.h"
+#include "builtin_functions/divide/Divide.h"
+
+TEST(add_test, ShouldDivideNumbers) {
+    auto expression = {
+            SyntaxTreeNode(Token(Token::Integer, "8")),
+            SyntaxTreeNode(Token(Token::Integer, "2"))
+    };
+
+    auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "4"));
+    auto actual = Divide().evaluate(expression);
+
+    EXPECT_EQ(expectedResult == actual, true);
+}
+
+TEST(add_test, ShouldEvaluateNestedDivision) {
+    auto expression = {
+            SyntaxTreeNode(Token(Token::Integer, "18")),
+            SyntaxTreeNode(
+                    Token(Token::Integer, "/"),
+                    {
+                            SyntaxTreeNode(Token(Token::Integer, "4")),
+                            SyntaxTreeNode(Token(Token::Integer, "2"))
+                    }
+            )
+    };
+
+    auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "9"));
+    auto actual = Divide().evaluate(expression);
+
+    EXPECT_EQ(expectedResult == actual, true);
+}
