2261e147638e20b0bcb762d477ca9b8298292988
mrunix00 <mrunix00@protonmail.com>
2023-11-28 10:11:43 +0100

    tests: add unit tests for Add()

---
+++ tests/add_test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "parser/SyntaxTreeNode.h"
+#include "builtin_functions/add/add.h"
+
+TEST(add_test, ShouldAddTwoNumbers) {
+    auto expression = {
+            SyntaxTreeNode(Token(Token::Integer, "1")),
+            SyntaxTreeNode(Token(Token::Integer, "2"))
+    };
+
+    auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "3"));
+    auto actual = Add().evaluate(expression);
+
+    EXPECT_EQ(expectedResult == actual, true);
+}
+
+TEST(add_test, ShouldEvaluateNestedExpression) {
+    auto expression = {
+            SyntaxTreeNode(Token(Token::Integer, "1")),
+            SyntaxTreeNode(
+                    Token(Token::Integer, "+"),
+                    {
+                            SyntaxTreeNode(Token(Token::Integer, "2")),
+                            SyntaxTreeNode(Token(Token::Integer, "3"))
+                    }
+            )
+    };
+
+    auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "6"));
+    auto actual = Add().evaluate(expression);
+
+    EXPECT_EQ(expectedResult == actual, true);
+}
