ed8c1f7f279c1c401f68d9e9d4f4234ef4b3bc32
Mr.UNIX <mrunix00@protonmail.com>
2023-12-02 20:52:20 +0100

    fix: error handling for invalid numbers in Divide()

--- tests/divide.cpp
+++ tests/divide.cpp
@@ -1,4 +1,5 @@
 #include "builtin_functions/divide/Divide.h"
+#include "exceptions/SyntaxError.h"
 #include "parser/SyntaxTreeNode.h"
 #include <gtest/gtest.h>
 
@@ -24,3 +25,30 @@ TEST(add_test, ShouldEvaluateNestedDivision) {
 
     EXPECT_EQ(expectedResult == actual, true);
 }
+
+TEST(add_test, ThrowExceptionOnDivisionWithInvalidArguments) {
+    bool isCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+
+    const std::vector<SyntaxTreeNode> expression = {
+        SyntaxTreeNode(Token(Token::Integer, "1")),
+        SyntaxTreeNode(Token(Token::String, "\"Hello World\"", 2, 3)),
+    };
+
+    try {
+        Divide().evaluate(expression);
+    } catch (SyntaxError &error) {
+        isCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(isCaught, true);
+    EXPECT_EQ(errorMessage == "\"Hello World\" is not a number", true);
+    EXPECT_EQ(line == expression[1].token.line, true);
+    EXPECT_EQ(column == expression[1].token.column, true);
+}
+
