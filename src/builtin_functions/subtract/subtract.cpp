766f350313219b5e48b4ea028fa47d4f7f7706a3
Mr.UNIX <mrunix00@protonmail.com>
2023-12-02 20:49:36 +0100

    fix: error handling for invalid numbers in Subtract()

--- src/builtin_functions/subtract/subtract.cpp
+++ src/builtin_functions/subtract/subtract.cpp
@@ -1,11 +1,16 @@
 #include "subtract.h"
 #include "eval/eval.h"
+#include "exceptions/SyntaxError.h"
 
 SyntaxTreeNode Subtract::evaluate(const std::vector<SyntaxTreeNode> &args) {
     int result = std::stoi(Evaluate::evaluate(args[0]).token.token);
 
     for (int i = 1; i < args.size(); i++) {
         auto arg = Evaluate::evaluate(args[i]).token;
+        if (arg.type != Token::Integer) {
+            throw SyntaxError(args[i].token.token + " is not a number",
+                              args[i].token.line, args[i].token.column);
+        }
         result -= std::stoi(arg.token);
     }
 
--- tests/subtract_test.cpp
+++ tests/subtract_test.cpp
@@ -1,4 +1,5 @@
 #include "builtin_functions/subtract/subtract.h"
+#include "exceptions/SyntaxError.h"
 #include "parser/SyntaxTreeNode.h"
 #include <gtest/gtest.h>
 
@@ -23,4 +24,30 @@ TEST(add_test, ShouldEvaluateNestedSubtraction) {
     auto actual = Subtract().evaluate(expression);
 
     EXPECT_EQ(expectedResult == actual, true);
+}
+
+TEST(add_test, ThrowExceptionOnInvalidArgumentsInSubtraction) {
+    bool isCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+
+    const std::vector<SyntaxTreeNode> expression = {
+            SyntaxTreeNode(Token(Token::Integer, "1")),
+            SyntaxTreeNode(Token(Token::String, "\"Hello World\"", 2, 3)),
+    };
+
+    try {
+        Subtract().evaluate(expression);
+    } catch (SyntaxError &error) {
+        isCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(isCaught, true);
+    EXPECT_EQ(errorMessage == "\"Hello World\" is not a number", true);
+    EXPECT_EQ(line == expression[1].token.line, true);
+    EXPECT_EQ(column == expression[1].token.column, true);
 }
