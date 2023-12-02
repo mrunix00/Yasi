cdbfa1d14991e5ba67851e851c538ce00448ae35
Mr.UNIX <mrunix00@protonmail.com>
2023-12-02 20:29:33 +0100

    fix: error handling for invalid numbers in Add()

--- src/builtin_functions/add/add.cpp
+++ src/builtin_functions/add/add.cpp
@@ -1,11 +1,16 @@
 #include "add.h"
 #include "eval/eval.h"
+#include "exceptions/SyntaxError.h"
 
 SyntaxTreeNode Add::evaluate(const std::vector<SyntaxTreeNode> &args) {
     int result = 0;
 
     for (const auto &arg: args) {
         auto evArg = Evaluate::evaluate(arg).token;
+        if (evArg.type != Token::Integer) {
+            throw SyntaxError(arg.token.token + " is not a number",
+                              arg.token.line, arg.token.column);
+        }
         result += std::stoi(evArg.token);
     }
 
--- tests/add_test.cpp
+++ tests/add_test.cpp
@@ -1,10 +1,14 @@
 #include "builtin_functions/add/add.h"
+#include "exceptions/SyntaxError.h"
 #include "parser/SyntaxTreeNode.h"
 #include <gtest/gtest.h>
 
 TEST(add_test, ShouldAddTwoNumbers) {
-    auto expression = {SyntaxTreeNode(Token(Token::Integer, "1")),
-                       SyntaxTreeNode(Token(Token::Integer, "2"))};
+    // (+ 1 2)
+    auto expression = {
+            SyntaxTreeNode(Token(Token::Integer, "1")),
+            SyntaxTreeNode(Token(Token::Integer, "2")),
+    };
 
     auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "3"));
     auto actual = Add().evaluate(expression);
@@ -13,14 +17,45 @@ TEST(add_test, ShouldAddTwoNumbers) {
 }
 
 TEST(add_test, ShouldEvaluateNestedAddition) {
+    // (+ 1 (+ 2 3))
     auto expression = {
             SyntaxTreeNode(Token(Token::Integer, "1")),
-            SyntaxTreeNode(Token(Token::Integer, "+"),
-                           {SyntaxTreeNode(Token(Token::Integer, "2")),
-                            SyntaxTreeNode(Token(Token::Integer, "3"))})};
+            SyntaxTreeNode(
+                    Token(Token::Integer, "+"),
+                    {
+                            SyntaxTreeNode(Token(Token::Integer, "2")),
+                            SyntaxTreeNode(Token(Token::Integer, "3")),
+                    }),
+    };
 
     auto expectedResult = SyntaxTreeNode(Token(Token::Integer, "6"));
     auto actual = Add().evaluate(expression);
 
     EXPECT_EQ(expectedResult == actual, true);
+}
+
+TEST(add_test, ThrowExceptionOnInvalidArguments) {
+    bool isCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+
+    const std::vector<SyntaxTreeNode> expression = {
+        SyntaxTreeNode(Token(Token::Integer, "1")),
+        SyntaxTreeNode(Token(Token::String, "\"Hello World\"", 2, 3)),
+    };
+
+    try {
+        Add().evaluate(expression);
+    } catch (SyntaxError &error) {
+        isCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(isCaught, true);
+    EXPECT_EQ(errorMessage == "\"Hello World\" is not a number", true);
+    EXPECT_EQ(line == expression[1].token.line, true);
+    EXPECT_EQ(column == expression[1].token.column, true);
 }
