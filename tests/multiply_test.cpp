f380692293b5a0fe15cfcb492e278d2c4caadd24
Mr.UNIX <mrunix00@protonmail.com>
2023-12-02 20:35:11 +0100

    fix: error handling for invalid numbers in Multiply()

--- tests/multiply_test.cpp
+++ tests/multiply_test.cpp
@@ -1,4 +1,5 @@
 #include "builtin_functions/multiply/multiply.h"
+#include "exceptions/SyntaxError.h"
 #include "parser/SyntaxTreeNode.h"
 #include <gtest/gtest.h>
 
@@ -23,4 +24,30 @@ TEST(add_test, ShouldEvaluateNestedMultiplication) {
     auto actual = Multiply().evaluate(expression);
 
     EXPECT_EQ(expectedResult == actual, true);
+}
+
+TEST(add_test, ThrowExceptionOnMultiplyWithInvalidArguments) {
+    bool isCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+
+    const std::vector<SyntaxTreeNode> expression = {
+        SyntaxTreeNode(Token(Token::Integer, "1")),
+        SyntaxTreeNode(Token(Token::String, "\"Hello World\"", 2, 3)),
+    };
+
+    try {
+        Multiply().evaluate(expression);
+    } catch (SyntaxError &error) {
+        isCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(isCaught, true);
+    EXPECT_EQ(errorMessage == "\"Hello World\" is not a number", true);
+    EXPECT_EQ(line == expression[1].token.line, true);
+    EXPECT_EQ(column == expression[1].token.column, true);
 }
