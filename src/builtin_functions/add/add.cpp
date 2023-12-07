e356532da308dadee57cc037be963e018b45f980
Mr.UNIX <mrunix00@protonmail.com>
2023-12-07 21:20:12 +0100

    refactor: code organisation in Add::evaluate()

--- src/builtin_functions/add/add.cpp
+++ src/builtin_functions/add/add.cpp
@@ -1,30 +1,25 @@
 #include "add.h"
 #include "eval/eval.h"
 #include "exceptions/SyntaxError.h"
+#include "lexer/Lexer.h"
 #include <sstream>
 #include <string>
 
 SyntaxTreeNode Add::evaluate(const std::vector<SyntaxTreeNode> &args) {
-    int int_res = 0;
-    float dec = 0;
+    float result = 0;
 
     for (const auto &arg: args) {
         auto evArg = Evaluate::evaluate(arg).token;
-        if (evArg.type == Token::Integer) {
-            int_res += std::stoi(evArg.token);
-        } else if (evArg.type == Token::Decimal) {
-            dec += std::stof(evArg.token);
+        if (evArg.type == Token::Integer || evArg.type == Token::Decimal) {
+            result += std::stof(evArg.token);
         } else {
             throw SyntaxError(arg.token.token + " is not a number",
                               arg.token.line, arg.token.column);
         }
     }
 
-    if (dec == 0) {
-        return SyntaxTreeNode(Token(Token::Integer, std::to_string(int_res)));
-    } else {
-        std::stringstream s;
-        s << (float) int_res + dec;
-        return SyntaxTreeNode(Token(Token::Decimal, s.str()));
-    }
+    std::stringstream s;
+    s << result;
+    if (int(result) != result) return Token(Token::Decimal, s.str());
+    return Token(Token::Integer, s.str());
 }
--- src/parser/SyntaxTreeNode.h
+++ src/parser/SyntaxTreeNode.h
@@ -14,7 +14,7 @@ public:
     SyntaxTreeNode(Token token, const std::vector<SyntaxTreeNode> &children)
         : token(std::move(token)), children(children) {}
 
-    explicit SyntaxTreeNode(Token token) : token(std::move(token)) {}
+    SyntaxTreeNode(Token token) : token(std::move(token)) {}
 
     bool operator==(const SyntaxTreeNode &node) const {
         return token == node.token && children == node.children;
