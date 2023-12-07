929e357a677ec5046497b7d74aa6825cfeb7868f
Mr.UNIX <mrunix00@protonmail.com>
2023-12-07 21:23:21 +0100

    refactor: code organisation in Multiply::evaluate()

--- src/builtin_functions/multiply/multiply.cpp
+++ src/builtin_functions/multiply/multiply.cpp
@@ -5,26 +5,19 @@
 #include <string>
 
 SyntaxTreeNode Multiply::evaluate(const std::vector<SyntaxTreeNode> &args) {
-    int int_result = 1;
-    float dec_result = 1;
-
+    float result = 1;
     for (const auto &arg: args) {
         auto evArg = Evaluate::evaluate(arg).token;
-        if (evArg.type == Token::Integer) {
-            int_result *= std::stoi(evArg.token);
-        } else if (evArg.type == Token::Decimal) {
-            dec_result *= std::stof(evArg.token);
+        if (evArg.type == Token::Integer || evArg.type == Token::Decimal) {
+            result *= std::stof(evArg.token);
         } else {
             throw SyntaxError(arg.token.token + " is not a number",
                               arg.token.line, arg.token.column);
         }
     }
 
-    if (dec_result != 1) {
-        std::stringstream s;
-        s << (float) int_result * dec_result;
-        return SyntaxTreeNode(Token(Token::Decimal, s.str()));
-    } else {
-        return SyntaxTreeNode(Token(Token::Integer, std::to_string(int_result)));
-    }
+    std::stringstream s;
+    s << result;
+    if (int(result) != result) return Token(Token::Decimal, s.str());
+    return Token(Token::Integer, s.str());
 }
