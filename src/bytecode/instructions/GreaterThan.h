258fe42100a001a01397a59a9627da37676d6cdd
Mr.UNIX <mrunix00@protonmail.com>
2024-02-04 21:39:55 +0100

    feat: implement GreaterThan instruction

--- src/bytecode/instructions/GreaterThan.h
+++ src/bytecode/instructions/GreaterThan.h
@@ -9,7 +9,16 @@ namespace Bytecode {
     class GreaterThan final : public Instruction {
     public:
         GreaterThan() { type = InstructionType::GreaterThan; };
-        void execute(Bytecode::VM *vm) override {}
+        void execute(Bytecode::VM *vm) override {
+            auto object1 = vm->stackPop();
+            auto object2 = vm->stackPop();
+            auto result = new StackObject(
+                    new Literal(object2->literal->int_literal >
+                                                object1->literal->int_literal
+                                        ? Boolean::True
+                                        : Boolean::False));
+            vm->stackPush(result);
+        }
         [[nodiscard]] std::string toString() const override { return "GreaterThan"; }
         bool operator==(const Instruction &instruction) const override {
             return instruction.type == type;
