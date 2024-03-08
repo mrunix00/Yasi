2ac35f2d4393277768c8dfca649f0d4a1b01fdda
Mr.UNIX <mrunix00@protonmail.com>
2024-03-08 18:29:56 +0100

    refactor, perf: move all method delcaration of Stack to Stach.h

--- src/bytecode/vm/Stack.h
+++ src/bytecode/vm/Stack.h
@@ -19,15 +19,114 @@ namespace Bytecode {
             used = 0;
         }
 
-        void push(ObjectType, uint8_t *data, size_t amount);
-        void push(const StackObject &);
-        void push(double data);
-        void push(bool data);
-        void push(const std::string &data);
+        void push(ObjectType type, uint8_t *data, size_t amount) {
+            if ((used + amount + 1) > capacity) {
+                capacity *= 2;
+                stackData = (uint8_t *) realloc(stackData, capacity);
+            }
+            switch (type) {
+                case String:
+                    *((char **) stackTop) = strdup((char *) data);
+                    break;
+                case Number:
+                    *((double *) stackTop) = *((double *) data);
+                    break;
+                case Boolean:
+                    *stackTop = *data;
+                    break;
+                default:
+                    break;
+            }
+            stackTop += amount;
+            *(stackTop++) = type;
+            used += amount + 1;
+        }
 
-        StackObject pop();
-        StackObject top();
+        void push(double number) {
+            push(ObjectType::Number,
+                 (uint8_t *) &number,
+                 sizeof(double));
+        }
 
-        void clear();
+        void push(bool boolean) {
+            push(ObjectType::Boolean,
+                 (uint8_t *) &boolean,
+                 1);
+        }
+
+        void push(const std::string &str) {
+            push(ObjectType::String,
+                 (uint8_t *) str.c_str(),
+                 sizeof(char *));
+        }
+
+        void push(const StackObject &object) {
+            switch (object.type) {
+                case ObjectType::Boolean:
+                    push(object.asBoolean());
+                    break;
+                case ObjectType::Number:
+                    push(object.asNumber());
+                    break;
+                case ObjectType::String:
+                    push(object.asString());
+                    break;
+            }
+        }
+        StackObject pop() {
+            uint8_t type = *(--stackTop);
+            StackObject object{};
+            size_t size;
+            switch (type) {
+                case ObjectType::Boolean:
+                    size = 1;
+                    object = StackObject(*((bool *) (stackTop - size)));
+                    break;
+                case ObjectType::Number:
+                    size = sizeof(double);
+                    object = StackObject(*((double *) (stackTop - size)));
+                    break;
+                case ObjectType::String:
+                    size = sizeof(char *);
+                    object = StackObject(std::string(*((char **) (stackTop - size))));
+                    break;
+                default:
+                    size = -1;
+            }
+
+            stackTop -= size;
+            used = used - size - 1;
+
+            return object;
+        }
+
+        StackObject top() {
+            uint8_t type = *(stackTop - 1);
+
+            if (!used)
+                return StackObject{};
+
+            StackObject object{};
+            size_t size;
+
+            switch (type) {
+                case ObjectType::Boolean:
+                    size = 1;
+                    return StackObject(*((bool *) (stackTop - size - 1)));
+                case ObjectType::Number:
+                    size = sizeof(double);
+                    return StackObject(*((double *) (stackTop - size - 1)));
+                case ObjectType::String:
+                    size = sizeof(char *);
+                    return StackObject(std::string(*((char **) (stackTop - size - 1))));
+                default:
+                    return object;
+            }
+        }
+
+        void clear() {
+            stackTop = stackData;
+            used = 0;
+        }
     };
-}
+}// namespace Bytecode
