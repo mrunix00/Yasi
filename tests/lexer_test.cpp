2ac461cac166739fc9bcc8105e428dc157490ffe
Mr.UNIX <mrunix00@protonmail.com>
2024-03-14 21:21:46 +0100

    tests: undelete parsing and lexing tests

---
+++ tests/lexer_test.cpp
@@ -0,0 +1,174 @@
+#include "../src/lexer/Lexer.h"
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(lex_test, ShouldTokenizeSingleToken) {
+    const std::string sample = "32";
+    const std::vector expected = {new Token(Token::Number, "32")};
+    const auto actual = Lexer::tokenize(sample);
+    EXPECT_EQ(*expected[0] == *actual[0], true);
+}
+
+TEST(lex_test, ShouldTokenizeSingleChars) {
+    const std::string sample = "(+ 1 2)";
+    const std::vector tokens = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "2"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto result = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens.size() == result.size(), true);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(*tokens[i] == *result[i], true);
+    }
+}
+
+TEST(lex_test, ShouldTokenizeMultipleChars) {
+    const std::string sample = "(multiply 1 2)";
+    const std::vector tokens = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "multiply"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "2"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto result = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens.size() == result.size(), true);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(*tokens[i] == *result[i], true);
+    }
+}
+
+TEST(lex_test, ShouldTokenizeLargeNumbers) {
+    const std::string sample = "(+ 13 54)";
+    const std::vector tokens = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "13"),
+            new Token(Token::Number, "54"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto result = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens.size() == result.size(), true);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(*tokens[i] == *result[i], true);
+    }
+}
+
+TEST(lex_test, ShouldTokenizeNegativeNumbers) {
+    const std::string sample = "(+ 1 -1)";
+    const std::vector tokens = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "-1"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto result = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens.size() == result.size(), true);
+    for (int i = 0; i < tokens.size(); i++) {
+        EXPECT_EQ(*tokens[i] == *result[i], true);
+    }
+}
+
+TEST(lex_test, StringsToken) {
+    const std::string sample = "(print \"Hello World\")";
+    const std::vector expected = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "print"),
+            new Token(Token::String, "\"Hello World\""),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto actual = Lexer::tokenize(sample);
+
+    EXPECT_EQ(expected.size() == actual.size(), true);
+    for (int i = 0; i < expected.size(); i++) {
+        EXPECT_EQ(*expected[i] == *actual[i], true);
+    }
+}
+
+TEST(lex_test, DecimalTokens) {
+    const std::string sample = "(+ 1.2 3)";
+    const std::vector expected = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "1.2"),
+            new Token(Token::Number, "3"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto actual = Lexer::tokenize(sample);
+
+    EXPECT_EQ(expected.size() == actual.size(), true);
+    for (int i = 0; i < expected.size(); i++) {
+        EXPECT_EQ(*expected[i] == *actual[i], true);
+    }
+}
+
+TEST(lex_test, ShouldLocateTokens) {
+    const std::string sample = "(+ 12 54)";
+    const auto tokens = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens[0]->line == 1, true);
+    EXPECT_EQ(tokens[0]->column, true);
+
+    EXPECT_EQ(tokens[1]->line == 1, true);
+    EXPECT_EQ(tokens[1]->column == 2, true);
+
+    EXPECT_EQ(tokens[2]->line == 1, true);
+    EXPECT_EQ(tokens[2]->column == 4, true);
+
+    EXPECT_EQ(tokens[3]->line == 1, true);
+    EXPECT_EQ(tokens[3]->column == 7, true);
+
+    EXPECT_EQ(tokens[4]->line == 1, true);
+    EXPECT_EQ(tokens[4]->column == 9, true);
+}
+
+TEST(lex_test, ShouldLocateTokensOnMultiLineExpressions) {
+    const std::string sample = "(+\n1 2\n)";
+    const auto tokens = Lexer::tokenize(sample);
+
+    EXPECT_EQ(tokens[0]->line == 1, true);
+    EXPECT_EQ(tokens[0]->column == 1, true);
+
+    EXPECT_EQ(tokens[1]->line == 1, true);
+    EXPECT_EQ(tokens[1]->column == 2, true);
+
+    EXPECT_EQ(tokens[2]->line == 2, true);
+    EXPECT_EQ(tokens[2]->column == 1, true);
+
+    EXPECT_EQ(tokens[3]->line == 2, true);
+    EXPECT_EQ(tokens[3]->column == 3, true);
+
+    EXPECT_EQ(tokens[4]->line == 3, true);
+    EXPECT_EQ(tokens[4]->column == 1, true);
+}
+
+TEST(lex_test, ShouldTokenizeAStringWithParenthesis) {
+    const std::string sample = "(print \"fib(10)\")";
+
+    const std::vector expected = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "print"),
+            new Token(Token::String, "\"fib(10)\""),
+            new Token(Token::ClosedBracket, ")"),
+    };
+    const auto actual = Lexer::tokenize(sample);
+
+    EXPECT_EQ(expected.size() == actual.size(), true);
+    for (int i = 0; i < expected.size(); i++) {
+        EXPECT_EQ(*expected[i] == *actual[i], true);
+    }
+}
---
+++ tests/parse_test.cpp
@@ -0,0 +1,228 @@
+#include "../src/parser/Parser.h"
+#include "exceptions/SyntaxError.h"
+#include <gtest/gtest.h>
+
+TEST(parse_test, ReturnNullWhenGivenNoTokens) {
+    const auto actual = Parser::parse({});
+    EXPECT_EQ(actual == nullptr, true);
+}
+
+TEST(parse_test, ParseSingleToken) {
+    const auto sample = {new Token(Token::Number, "2")};
+
+    const auto expected = SyntaxTreeNode(Token(Token::Number, "2"));
+    const auto actual = *Parser::parse(sample);
+
+    EXPECT_EQ(expected == actual, true);
+}
+
+TEST(parse_test, ParseSimpleOperation) {
+    // ( + 1 2 )
+    const auto sample = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "2"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+    const auto expected = SyntaxTreeNode(
+            Token(Token::Symbol, "+"),
+            {
+                    new SyntaxTreeNode(Token(Token::Number, "1")),
+                    new SyntaxTreeNode(Token(Token::Number, "2")),
+            });
+
+    const auto actual = *Parser::parse(sample);
+
+    EXPECT_EQ(actual == expected, true);
+}
+
+TEST(parse_test, ParseSimpleOperationWithStringArgument) {
+    // ( print "Hello World" )
+    const auto sample = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "print"),
+            new Token(Token::String, "\"Hello World\""),
+            new Token(Token::ClosedBracket, ")"),
+    };
+    const auto expected = SyntaxTreeNode(
+            Token(Token::Symbol, "print"),
+            {
+                    new SyntaxTreeNode(Token(Token::String, "\"Hello World\"")),
+            });
+
+    const auto actual = *Parser::parse(sample);
+
+    EXPECT_EQ(actual == expected, true);
+}
+
+TEST(parse_test, ParseNestedOperation) {
+    // ( + ( + 1 3 ) 2 )
+    const auto sample = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "3"),
+            new Token(Token::ClosedBracket, ")"),
+            new Token(Token::Number, "2"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto expected = SyntaxTreeNode(
+            Token(Token::Symbol, "+"),
+            {
+                    new SyntaxTreeNode(
+                            Token(Token::Symbol, "+"),
+                            {
+                                    new SyntaxTreeNode(Token(Token::Number, "1")),
+                                    new SyntaxTreeNode(Token(Token::Number, "3")),
+                            }),
+                    new SyntaxTreeNode(Token(Token::Number, "2")),
+            });
+
+    const auto actual = *Parser::parse(sample);
+
+    EXPECT_EQ(actual == expected, true);
+}
+
+TEST(parse_test, ParseDeeplyNestedOperation) {
+    // ( + ( * 2 3 ) ( + ( * 1 2 ) 4 ) 7 )
+    const auto sample = {
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "*"),
+            new Token(Token::Number, "2"),
+            new Token(Token::Number, "3"),
+            new Token(Token::ClosedBracket, ")"),
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "+"),
+            new Token(Token::OpenBracket, "("),
+            new Token(Token::Symbol, "*"),
+            new Token(Token::Number, "1"),
+            new Token(Token::Number, "2"),
+            new Token(Token::ClosedBracket, ")"),
+            new Token(Token::Number, "4"),
+            new Token(Token::ClosedBracket, ")"),
+            new Token(Token::Number, "7"),
+            new Token(Token::ClosedBracket, ")"),
+    };
+
+    const auto expected = SyntaxTreeNode(
+            Token(Token::Symbol, "+"),
+            {
+                    new SyntaxTreeNode(
+                            Token(Token::Symbol, "*"),
+                            {
+                                    new SyntaxTreeNode(Token(Token::Number, "2")),
+                                    new SyntaxTreeNode(Token(Token::Number, "3")),
+                            }),
+                    new SyntaxTreeNode(
+                            Token(Token::Symbol, "+"),
+                            {
+                                    new SyntaxTreeNode(
+                                            Token(Token::Symbol, "*"),
+                                            {
+                                                    new SyntaxTreeNode(Token(Token::Number, "1")),
+                                                    new SyntaxTreeNode(Token(Token::Number, "2")),
+                                            }),
+                                    new SyntaxTreeNode(Token(Token::Number, "4")),
+                            }),
+                    new SyntaxTreeNode(Token(Token::Number, "7")),
+            });
+
+    const auto actual = *Parser::parse(sample);
+
+    EXPECT_EQ(actual == expected, true);
+}
+
+TEST(parse_test, ShouldThrowExceptionsOnExtraParenthesis) {
+    auto exceptionCaught = false;
+    const auto expectedErrorMessage = "Unexpected parenthesis -> ')'";
+    std::string actualErrorMessage;
+    int extraParenthesisLine;
+    int extraParenthesisColumn;
+
+    // (+ 1 2 ))
+    const std::vector<Token *> sample = {
+            new Token(Token::OpenBracket, "(", 1, 1),
+            new Token(Token::Symbol, "+", 1, 2),
+            new Token(Token::Number, "1", 1, 4),
+            new Token(Token::Number, "2", 1, 6),
+            new Token(Token::ClosedBracket, ")", 1, 8),
+            new Token(Token::ClosedBracket, ")", 1, 9),
+    };
+
+    try {
+        Parser::parse(sample);
+    } catch (SyntaxError &error) {
+        exceptionCaught = true;
+        actualErrorMessage = error.message;
+        extraParenthesisLine = error.line;
+        extraParenthesisColumn = error.column;
+    }
+
+    EXPECT_EQ(exceptionCaught, true);
+    EXPECT_EQ(expectedErrorMessage == actualErrorMessage, true);
+    EXPECT_EQ(extraParenthesisLine == sample[5]->line, true);
+    EXPECT_EQ(extraParenthesisColumn == sample[5]->column, true);
+}
+
+TEST(parse_test, ShouldThrowExceptionsOnMissingOperator) {
+    bool exceptionCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+    // ((+ 1 2))
+    const std::vector sample = {
+            new Token(Token::OpenBracket, "(", 1, 1),
+            new Token(Token::OpenBracket, "(", 1, 2),
+            new Token(Token::Symbol, "+", 1, 3),
+            new Token(Token::Number, "1", 1, 5),
+            new Token(Token::Number, "2", 1, 7),
+            new Token(Token::ClosedBracket, ")", 1, 8),
+            new Token(Token::ClosedBracket, ")", 1, 9),
+    };
+
+    try {
+        Parser::parse(sample);
+    } catch (SyntaxError &error) {
+        exceptionCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(exceptionCaught, true);
+    EXPECT_EQ(errorMessage == "An atom was expected after the opening parenthesis but not found", true);
+    EXPECT_EQ(line == sample[0]->line, true);
+    EXPECT_EQ(column == sample[0]->column, true);
+}
+
+TEST(parse_test, ShouldThrowExceptionsOnEmptyParenthesis) {
+    bool exceptionCaught = false;
+    std::string errorMessage;
+    int line = 0;
+    int column = 0;
+    // ((+ 1 2))
+    const std::vector<Token *> sample = {
+            new Token(Token::OpenBracket, "(", 1, 1),
+            new Token(Token::ClosedBracket, ")", 1, 2),
+    };
+
+    try {
+        Parser::parse(sample);
+    } catch (SyntaxError &error) {
+        exceptionCaught = true;
+        errorMessage = error.message;
+        line = error.line;
+        column = error.column;
+    }
+
+    EXPECT_EQ(exceptionCaught, true);
+    EXPECT_EQ(errorMessage == "An atom was expected after the opening parenthesis but not found", true);
+    EXPECT_EQ(line == sample[0]->line, true);
+    EXPECT_EQ(column == sample[0]->column, true);
+}
