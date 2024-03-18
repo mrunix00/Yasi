5cb33eced1ea83a30036e45060ba428638c99ae6
Mr.UNIX <mrunix00@protonmail.com>
2024-03-18 18:55:17 +0100

    test: add another test for lambda functions

--- tests/lambda_test.cpp
+++ tests/lambda_test.cpp
@@ -10,3 +10,15 @@ TEST(lambda_test, ShouldDefineALambdaFunctionAndCallIt) {
 
     EXPECT_EQ(actual_result, expected_result);
 }
+
+TEST(lambda_test, ShouldPassLambdaAsArgument) {
+    const auto expression =
+            "(define add (lambda (a b) (+ a b)))\n"
+            "(define (calc f a b) (f a b))\n"
+            "(calc add 5 6)";
+
+    const auto expected_result = StackObject((double) 11);
+    const auto actual_result = run_program(expression);
+
+    EXPECT_EQ(actual_result, expected_result);
+}
