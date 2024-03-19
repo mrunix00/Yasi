57b2be668c088e73842a6c1a6d0311ee11a84a24
Mr.UNIX <mrunix00@protonmail.com>
2024-03-19 14:54:21 +0100

    fix: error handling for memory allocation failures in CallStack

--- src/bytecode/vm/CallStack.h
+++ src/bytecode/vm/CallStack.h
@@ -4,6 +4,7 @@
 #include "bytecode/objects/StackObject.h"
 #include <cstdint>
 #include <cstdlib>
+#include <iostream>
 
 namespace Bytecode {
     struct StackFrame {
@@ -28,9 +29,16 @@ namespace Bytecode {
             stackframe_capacity = 512;
             local_registers_capacity = 512;
             local_registers_used = 0;
+
             local_registers = (StackObject *) malloc(local_registers_capacity *
                                                      sizeof(StackObject));
             stack = (StackFrame *) malloc(stackframe_capacity * sizeof(StackFrame));
+
+            if (local_registers == nullptr || stack == nullptr) {
+                std::cerr << "Failed to allocate memory for the call stack!" << std::endl;
+                exit(1);
+            }
+
             stackTop = stack;
             stackframe_used = 0;
             newStackFrame(0);
@@ -48,7 +56,12 @@ namespace Bytecode {
         void newStackFrame(const uint32_t segment, const uint32_t args, Stack *program_stack) {
             if (stackframe_used + 1 > stackframe_capacity) {
                 stackframe_capacity *= 2;
-                stack = (StackFrame *) realloc(stack, stackframe_capacity * sizeof(StackFrame));
+                auto temp = realloc(stack, stackframe_capacity * sizeof(StackFrame));
+                if (temp == nullptr) {
+                    std::cerr << "Failed to allocate memory for the call stack!" << std::endl;
+                    exit(1);
+                }
+                stack = (StackFrame *) temp;
             }
             stackTop = stack + stackframe_used;
             stack[stackframe_used++] = StackFrame{
@@ -59,14 +72,19 @@ namespace Bytecode {
 
             if (local_registers_used + args > local_registers_capacity) {
                 local_registers_capacity *= 2;
-                local_registers = (StackObject *)
-                        realloc(local_registers, local_registers_capacity *
-                                                         sizeof(StackObject));
+                auto temp = realloc(local_registers, local_registers_capacity *
+                                                             sizeof(StackObject));
+                if (temp == nullptr) {
+                    std::cerr << "Failed to allocate memory for the call stack!" << std::endl;
+                    exit(1);
+                }
+                local_registers = (StackObject *) temp;
             }
             local_registers_used += args;
 
-            for (auto i = args - 1; i + 1 != 0; i--)
-                setLocal(i, program_stack->pop());
+            if (program_stack != nullptr)
+                for (auto i = args - 1; i + 1 != 0; i--)
+                    setLocal(i, program_stack->pop());
         }
 
         void popStackFrame() {
