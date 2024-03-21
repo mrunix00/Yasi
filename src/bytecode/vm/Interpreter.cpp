fa21b0d806c8e1abe855a328a445a289dd3c8081
Mr.UNIX <mrunix00@protonmail.com>
2024-03-21 21:21:39 +0100

    fix: error handling for dividing numbers

--- src/bytecode/vm/Interpreter.cpp
+++ src/bytecode/vm/Interpreter.cpp
@@ -42,8 +42,15 @@ void Bytecode::Interpreter::execute(const Program &program) {
                 vm.program_stack.push(object1.asNumber() * object2.asNumber());
             } break;
             case InstructionType::Divide: {
-                const auto object2 = vm.program_stack.pop();
+                 const auto object2 = vm.program_stack.pop();
                 const auto object1 = vm.program_stack.pop();
+                if (object1.type != ObjectType::Number ||
+                    object2.type != ObjectType::Number) {
+                    throw SyntaxError("Invalid argument type for function \"/\", Expected number, got string");
+                }
+                if (object2.asNumber() == 0) {
+                    throw SyntaxError("Division by zero", 0, 0);
+                }
                 vm.program_stack.push(object1.asNumber() / object2.asNumber());
             } break;
             case InstructionType::Equals: {
--- tests/divide_test.cpp
+++ tests/divide_test.cpp
@@ -1,4 +1,5 @@
 #include "boilerplate.h"
+#include "exceptions/SyntaxError.h"
 
 TEST(divide_test, ShouldDivideTwoLiterals) {
     const auto program = "(/ 30 2)";
@@ -26,3 +27,53 @@ TEST(divide_test, ShouldAddDecimalAndIntegerNumbers) {
 
     EXPECT_EQ(actual_result, expected_result);
 }
+
+TEST(divide_test, ShouldReturnTheReciprocalOfTheNumberWhenDividingOneNumber) {
+    const auto expression = "(/ 2)";
+
+    const auto expected_result = StackObject((double) 0.5);
+    const auto actual_result = run_program(expression);
+
+    EXPECT_EQ(actual_result, expected_result);
+}
+
+TEST(divide_test, ShouldThrowAnErrorWhenDividingByZero) {
+    const auto expression = "(/ 2 0)";
+
+    std::string message;
+    try {
+    const auto actual_result = run_program(expression);
+    } catch (const SyntaxError& e) {
+        message = e.message;
+    }
+
+    EXPECT_EQ(message, "Division by zero");
+}
+
+TEST(divide_test, ShouldThrowAnErrorWhenNoArgsWereFound) {
+    const auto expression = "(/)";
+
+    std::string message;
+    try {
+        run_program(expression);
+    } catch (const SyntaxError& e) {
+        message = e.message;
+    }
+    EXPECT_EQ(message, "Invalid number of arguments for function \"/\", Expected at least 1, got 0");
+}
+
+TEST(divide_test, ShouldThrowAnErrorWhenInvalidArgumentIsPasseg) {
+    const auto expression = "(/ 10 \"Hello World\")";
+
+    bool errorIsThrown = false;
+    std::string message;
+    try {
+        run_program(expression);
+    } catch (const SyntaxError& e) {
+        errorIsThrown = true;
+        message = e.message;
+    }
+    EXPECT_TRUE(errorIsThrown);
+    EXPECT_EQ(message, "Invalid argument type for function \"/\", Expected number, got string");
+}
+
