58f7b475ccb3dc3214b21136d078bedd655a96d8
Mr.UNIX <mrunix00@protonmail.com>
2024-03-22 00:33:41 +0100

    fix: error handling for equality checks

--- src/bytecode/builtin_functions/EqualsFunction.h
+++ src/bytecode/builtin_functions/EqualsFunction.h
@@ -7,9 +7,16 @@ namespace Bytecode::BuiltinFunctions {
     class Equals : public Function {
         void compile(
                 const std::vector<SyntaxTreeNode *> &args,
-                Program& program,
+                Program &program,
                 std::vector<Instruction *> &instructions,
                 Segment *segment) override {
+            if (args.empty()) {
+                throw SyntaxError("Invalid number of arguments for function \"=\", Expected at least 1, got 0");
+            }
+            if (args.size() == 1) {
+                instructions.push_back(new LoadLiteral(new StackObject(true)));
+                return;
+            }
             args[0]->compile(segment, program, instructions);
             args[1]->compile(segment, program, instructions);
             instructions.push_back(new Bytecode::Equals());
--- tests/equals_test.cpp
+++ tests/equals_test.cpp
@@ -1,4 +1,5 @@
 #include "boilerplate.h"
+#include "exceptions/SyntaxError.h"
 
 TEST(equals_test, ShouldReturnTrueOnEquality) {
     const auto program = "(= 10 10)";
@@ -34,4 +35,30 @@ TEST(equals_test, ShouldCompareTwoDecimalNumbersAndReturnFalseOnInequality) {
     const auto actual_result = run_program(program);
 
     EXPECT_EQ(actual_result, expected_result);
+}
+
+TEST(equals_test, ShouldReturnTrueWhenComparingOneNumber) {
+    const auto program = "(= 2)";
+
+    const auto expected_result = StackObject(true);
+    const auto actual_result = run_program(program);
+
+    EXPECT_EQ(actual_result, expected_result);
+}
+
+TEST(equals_test, ShouldThrowAnErrorWhenNoArgsWereFound) {
+    const auto program = "(=)";
+
+    bool errorIsThrown = false;
+    std::string message;
+
+    try {
+        run_program(program);
+    } catch (const SyntaxError& e) {
+        errorIsThrown = true;
+        message = e.message;
+    }
+
+    EXPECT_TRUE(errorIsThrown);
+    EXPECT_EQ(message, "Invalid number of arguments for function \"=\", Expected at least 1, got 0");
 }
