5602242237423955f1bd0cd52f3ebbbb8c899ec1
Mr.UNIX <mrunix00@protonmail.com>
2024-03-22 00:36:04 +0100

    fix: error handling for greater than checks

--- tests/greater_than_test.cpp
+++ tests/greater_than_test.cpp
@@ -1,4 +1,5 @@
 #include "boilerplate.h"
+#include "exceptions/SyntaxError.h"
 
 TEST(greater_than_test, ShouldReturnTrueWhenGreaterThan) {
     const auto program = "(> 20 10)";
@@ -35,3 +36,30 @@ TEST(greater_than_test, ShouldCompareTwoDecimalNumbersAndReturnFalseWhenNotGreat
 
     EXPECT_EQ(actual_result, expected_result);
 }
+
+TEST(greater_than_test, ShouldReturnTrueWhenComparingOneNumber) {
+    const auto program = "(> 2)";
+
+    const auto expected_result = StackObject(true);
+    const auto actual_result = run_program(program);
+
+    EXPECT_EQ(actual_result, expected_result);
+}
+
+TEST(greater_than_test, ShouldThrowAnErrorWhenNoArgsWereFound) {
+    const auto program = "(>)";
+
+    bool errorIsThrown = false;
+    std::string message;
+
+    try {
+        run_program(program);
+    } catch (const SyntaxError& e) {
+        errorIsThrown = true;
+        message = e.message;
+    }
+
+    EXPECT_TRUE(errorIsThrown);
+    EXPECT_EQ(message, "Invalid number of arguments for function \">\", Expected at least 1, got 0");
+}
+
