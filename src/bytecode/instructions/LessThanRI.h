7ed9629c1cf46706dad6831b1935d7890e0d7205
Ibrahim KAIKAA <mrunix00@protonmail.com>
2024-04-20 12:19:15 +0100

    refactor: remove unnecessary check in LessThanRI instruction

--- src/bytecode/instructions/LessThanRI.h
+++ src/bytecode/instructions/LessThanRI.h
@@ -9,10 +9,9 @@ namespace Bytecode {
             type = InstructionType::LessThanRI;
         }
         void execute(Bytecode::VM &vm) override {
-            const auto object2 =params.ri_params.intermediate;
+            const auto object2 = params.ri_params.intermediate;
             const auto object1 = vm.call_stack.getLocal(params.ri_params.reg);
-            if (object1.type != ObjectType::Number ||
-                object2.type != ObjectType::Number) {
+            if (object1.type != ObjectType::Number) {
                 throw SyntaxError("Invalid argument type for function \"<\", Expected number, got string");
             }
             vm.program_stack.push(object1.asNumber() < object2.asNumber());
